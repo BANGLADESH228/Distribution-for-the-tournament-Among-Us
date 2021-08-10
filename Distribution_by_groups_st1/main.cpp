2cc21619361b628cc8582df672d0654fed4523c9
BANGLADESH228 <dias_bolatov10@mail.ru>
2021-08-10 23:45:22 +0500

    10.08.21

--- Distribution_by_groups_st1/main.cpp
+++ Distribution_by_groups_st1/main.cpp
@@ -4,15 +4,6 @@
 #include <QRandomGenerator64>
 #include <algorithm>
 
-//class Player
-//{
-//public:
-//    std::string name;
-//    int result_of_player;
-
-//    std::vector<Player> players = {};
-//};
-
 template<typename T>
 void print_new_vectors(std::vector<T> const &v)
 {
@@ -22,7 +13,7 @@ void print_new_vectors(std::vector<T> const &v)
     std::cout << std::endl;
 }
 
-void generate_random_members(int num, std::vector<int>& vec)
+void generate_random_numbers(int num, std::vector<int>& vec)
 {
     int rez;
 
@@ -35,7 +26,8 @@ void generate_random_members(int num, std::vector<int>& vec)
     }
 }
 
-void split_original_vec(int num_of_groups, int num_of_team_members, std::vector<int>& results)
+void split_original_and_print_vec(int num_of_groups,
+                        int num_of_team_members, std::vector<int>& results)
 {
     std::vector<int> new_vec[num_of_groups];
 
@@ -54,18 +46,53 @@ void split_original_vec(int num_of_groups, int num_of_team_members, std::vector<
     }
 }
 
+class Player
+{
+public:
+    Player(){};
+
+    Player(std::string name_, std::vector<int> scores_)
+    {
+       name = name_;
+       scores = scores_;
+    }
+
+    double calculate_result(std::vector<int> scores,
+                     double const percentage, int const num_of_scores)
+    {
+       generate_random_numbers(num_of_scores, scores);
+       auto remove_count = static_cast<size_t>(scores.size() * percentage + 0.5);
+
+       scores.erase(std::begin(scores), std::begin(scores) + remove_count);
+       scores.erase(std::end(scores) - remove_count, std::end(scores));
+
+       auto total = std::accumulate(std::cbegin(scores), std::cend(scores),
+                                    0ull, [](auto const sum, auto const e){ sum += e; });
+
+       return static_cast<double>(total) / scores.size();
+    }
+
+    ~Player(){};
+
+private:
+    std::string name;
+    std::vector<int> scores;
+};
+
 int main()
 {
-    //std::string name;
-    int num_of_peoples = 0, num_of_groups = 0, num_of_team_members = 0;
+    std::string name;
+    int num_of_peoples = 0, num_of_groups = 0, num_of_team_members = 0, num_of_scores = 0;
     std::vector<int> results = {};
+    std::vector<int> scores = {};
+    double percentage = 0;
 
-//    std::string names[30] = {
-//        "Anatoliy", "Katya",   "Masha",     "Eva",      "Zema",  "Slavik",    "Margo",  "Yri",      "Vladimir", "Evilina",
-//        "Alesha",   "Vasiliy", "Dima",      "Ruslan",   "Oleg",  "Mikhail",   "Igor",   "Ivan",     "Gleb",     "Denis",
-//        "Alina",    "Alisa",   "Anastasia", "Viktoria", "Darya", "Ekaterina", "Kamila", "Kristina", "Ksenia",   "Natalya",
-//    };
-//    name = names[rand() % 30];
+    std::string names[30] = {
+        "Anatoliy", "Katya",   "Masha",     "Eva",      "Zema",  "Slavik",    "Margo",  "Yri",      "Vladimir", "Evilina",
+        "Alesha",   "Vasiliy", "Dima",      "Ruslan",   "Oleg",  "Mikhail",   "Igor",   "Ivan",     "Gleb",     "Denis",
+        "Alina",    "Alisa",   "Anastasia", "Viktoria", "Darya", "Ekaterina", "Kamila", "Kristina", "Ksenia",   "Natalya",
+    };
+    name = names[rand() % 30];
 
     std::cout << "Enter a number - the number of participants: ";
     std::cin >> num_of_peoples;
@@ -74,7 +101,7 @@ int main()
 
     num_of_team_members = num_of_peoples / num_of_groups;
 
-    generate_random_members(num_of_peoples, results);
+    generate_random_numbers(num_of_peoples, results);
 
     if (num_of_peoples % num_of_groups == 0)
     {
@@ -84,7 +111,18 @@ int main()
     }
 
     std::cout << std::endl;
-    split_original_vec(num_of_groups, num_of_team_members, results);
+    split_original_and_print_vec(num_of_groups, num_of_team_members, results);
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+
+    std::cout << "Enter a number - the percentage: ";
+    std::cin >> percentage;
+    std::cout << "Enter a number - the number of scores: ";
+    std::cin >> num_of_scores;
+
+    Player player(name, scores);
+    player.calculate_result(scores, percentage, num_of_scores);
 
     return 0;
 }

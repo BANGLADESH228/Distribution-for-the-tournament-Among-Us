52dbfd703282da6586f66c8a2e66aa2df0ea7908
BANGLADESH228 <dias_bolatov10@mail.ru>
2021-08-11 16:42:40 +0500

    11.08.21

--- Distribution_by_groups_st1/main.cpp
+++ Distribution_by_groups_st1/main.cpp
@@ -15,15 +15,18 @@ void print_new_vectors(std::vector<T> const &v)
 
 void generate_random_numbers(int num, std::vector<int>& vec)
 {
-    int rez;
-
     for (int i = 0; i < num; ++i)
     {
         std::uniform_int_distribution<int> distribution(0, 10);
-        rez = distribution(*QRandomGenerator::global());
+        int rez = distribution(*QRandomGenerator::global());
         vec.push_back(rez);
         std::sort(vec.begin(), vec.end());
     }
+}
+
+double to_percentage(int percentage)
+{
+    return static_cast<double>(percentage / 100.0);
 }
 
 void split_original_and_print_vec(int num_of_groups,
@@ -35,6 +38,7 @@ void split_original_and_print_vec(int num_of_groups,
     {
         auto start_itr = std::next(results.cbegin(), i * num_of_team_members);
         auto end_itr = std::next(results.cbegin(), i * num_of_team_members + num_of_team_members);
+
         new_vec[i].resize(num_of_team_members);
         std::copy(start_itr, end_itr, new_vec[i].begin());
     }
@@ -57,17 +61,14 @@ public:
        scores = scores_;
     }
 
-    double calculate_result(std::vector<int> scores,
-                     double const percentage, int const num_of_scores)
+    double truncated_mean(std::vector<int> scores, double const percentage)
     {
-       generate_random_numbers(num_of_scores, scores);
        auto remove_count = static_cast<size_t>(scores.size() * percentage + 0.5);
-
        scores.erase(std::begin(scores), std::begin(scores) + remove_count);
        scores.erase(std::end(scores) - remove_count, std::end(scores));
 
        auto total = std::accumulate(std::cbegin(scores), std::cend(scores),
-                                    0ull, [](auto const sum, auto const e){ sum += e; });
+                                    0ull, [](auto const sum, auto const e){ return sum + e; });
 
        return static_cast<double>(total) / scores.size();
     }
@@ -81,18 +82,19 @@ private:
 
 int main()
 {
-    std::string name;
-    int num_of_peoples = 0, num_of_groups = 0, num_of_team_members = 0, num_of_scores = 0;
+    int num_of_peoples = 0, num_of_groups = 0,
+            num_of_team_members = 0, num_of_scores = 0, percentage = 0;
+
     std::vector<int> results = {};
     std::vector<int> scores = {};
-    double percentage = 0;
+    std::vector<Player> players;
 
     std::string names[30] = {
         "Anatoliy", "Katya",   "Masha",     "Eva",      "Zema",  "Slavik",    "Margo",  "Yri",      "Vladimir", "Evilina",
         "Alesha",   "Vasiliy", "Dima",      "Ruslan",   "Oleg",  "Mikhail",   "Igor",   "Ivan",     "Gleb",     "Denis",
         "Alina",    "Alisa",   "Anastasia", "Viktoria", "Darya", "Ekaterina", "Kamila", "Kristina", "Ksenia",   "Natalya",
     };
-    name = names[rand() % 30];
+    std::string name = names[rand() % 30];
 
     std::cout << "Enter a number - the number of participants: ";
     std::cin >> num_of_peoples;
@@ -115,14 +117,32 @@ int main()
 
     ////////////////////////////////////////////////////////////////////////////////////////
 
+    std::cout << std::endl;
 
     std::cout << "Enter a number - the percentage: ";
     std::cin >> percentage;
     std::cout << "Enter a number - the number of scores: ";
     std::cin >> num_of_scores;
 
-    Player player(name, scores);
-    player.calculate_result(scores, percentage, num_of_scores);
+    generate_random_numbers(num_of_scores, scores);
+    double new_percentage = to_percentage(percentage);
+    std::cout << "Percentage: " << new_percentage << std::endl;
+
+    for (int i = 0; i < num_of_peoples; ++i)
+    {
+        generate_random_numbers(num_of_scores, scores);
+        Player player(name, scores);
+        for (auto const& sc : scores)
+            std::cout << sc << ' ';
+        auto final_result = player.truncated_mean(scores, new_percentage);
+        std::cout << "Final: " << name << final_result << std::endl;
+    }
+
+    //Player player(name, scores);
+    //auto final_result = player.truncated_mean(scores, new_percentage);
+
+    //std::cout << std::endl;
+    //std::cout << "Final: " << final_result << std::endl;
 
     return 0;
 }
